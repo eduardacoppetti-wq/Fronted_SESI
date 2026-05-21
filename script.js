903b448fc143e742f5a72d00f2866fabe563cf5e
EduardaCoppetti <eduarda.coppetti@escola.pr.gov.br>
2026-05-21 12:11:45 -0300

    formulario de venda-versao 01

--- script.js
+++ script.js
@@ -20,15 +20,36 @@ function executarSistema() {
             msg.style.color = "#00ff88";
             
             // Desconto
-            let valor = (valor > 500 || cupom) ? valor * 0.85 : valor;
+            let valorFinal = (valor > 500 || cupom) ? valor * 0.85 : valor;
             
             // Estoque 
             let estoque = ["placa de Video", "Processador", "Memoria RAM"];
             lista.innerHTML = ""; //Limpa a lista anterior
-    
 
+            // forEach: Percorre um array e aplica uma ação para cada elemento 
+            estoque.forEach(item => {
+                let li = document.createElement("li");
+                li.innerText = `Item $(item) reservado.`;
+                lista.appendChild(li); // usado para adicionar um novo elemento ou texto
+            });
+
+            // Relatorio
+            relatorio.style.display = "block";
+            relatorio.innerHTML = `
+            <strong> RESUMO DO PEDIDO <\ strong><br>
+            Cliente: ${nome} <br>
+            total Original: R${valor.toFixed(2)} <br> 
+            <strong> Total com Desconto: R$ $ {valorFinal.totalfixed(2)} </strong>
+            `;
+
+
+
+
+        } else {
+            msg.innerText = "Venda bloqueada: Menor de 16 anos.";
+            msg.style.color = "#ff4444";
+            relatorio.style.display = "nome";
+            lista.innerHTML = "";
         }
 
-     
-
-}
+    }
